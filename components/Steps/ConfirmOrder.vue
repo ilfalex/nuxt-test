327018628b99ea5df6edf4f454b5d43ccd60cfce
Alexander Mayo <mayoalexandertd@icloud.com>
2019-07-17 11:25:18 -0500

    added the data to the confirmation page

--- components/Steps/ConfirmOrder.vue
+++ components/Steps/ConfirmOrder.vue
@@ -5,57 +5,73 @@
       :subtitle="subtitle"
       :step="step"
     />
-    <v-layout row wrap>
+
+    {{ product }}
+
+    <v-layout row wrap align-start>
       <v-flex
-        sm3
+        xs12
+        sm6
+        md3
         class="pa-4"
       >
         <v-card class="grey pa-2" width="100%">
           <h3>Anatomy</h3>
+          {{ productAnatomy }}
         </v-card>
       </v-flex>
       <v-flex
-        sm3
+        xs12
+        sm6
+        md3
         class="pa-4"
       >
         <v-card class="grey pa-2" width="100%">
           <h3>Texture</h3>
+          {{ productTexture }}
         </v-card>
       </v-flex>
       <v-flex
-        sm3
+        xs12
+        sm6
+        md3
         class="pa-4"
       >
         <v-card class="grey pa-2" width="100%">
           <h3>Sleve</h3>
+          {{ productSleve }}
         </v-card>
-      </v-flex>
+        </v-flex>
       <v-flex
-        sm3
+        xs12
+        sm6
+        md3
         class="pa-4"
       >
         <v-card class="grey pa-2" width="100%">
-          <h3>Submitted Anatomy</h3>
+          <h3>Model</h3>
+          {{ productModel }}
         </v-card>
       </v-flex>
 
       <v-flex
-        sm6
+        xs12
+        md6
         class="pa-4"
       >
         <v-card class="grey pa-2" width="100%">
           <h3>Submited Insignia</h3>
         </v-card>
       </v-flex>
-      <v-flex
-        sm6
+        <v-flex
+        xs12
+        md6
         class="pa-4"
       >
         <v-card class="grey pa-2" width="100%">
           <h3>Model</h3>
         </v-card>
       </v-flex>
-
     </v-layout>
   </div>
 </template>
@@ -73,8 +89,27 @@ export default {
   },
   computed: {
     product() {
-      console.log(this.$store.state.product)
       return this.$store.state.product
+    },
+    productAnatomy() {
+      return this.$store.state.options.anatomies.find(
+        item => item.id === this.product.anatomy
+      )
+    },
+    productTexture() {
+      return this.$store.state.options.anatomies.find(
+        item => item.id === this.product.texture
+      )
+    },
+    productSleve() {
+      return this.$store.state.options.anatomies.find(
+        item => item.id === this.product.sleve_cover
+      )
+    },
+    productModel() {
+      return this.$store.state.options.anatomies.find(
+        item => item.id === this.product.femlight_model
+      )
     }
   }
 }
