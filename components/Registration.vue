2f271e953fa6b60efb845e33c4d86366efb9340e
Alexander Mayo <mayoalexandertd@icloud.com>
2019-07-17 13:07:44 -0500

    flattened registration

--- components/Registration.vue
+++ components/Registration.vue
@@ -1,36 +1,34 @@
 <template>
-  <v-container fluid fill-height>
-    <v-layout align-center justify-center>
-      <v-flex xs12 sm8 md4>
-        <!-- <v-flex>
-          <div class="text-xs-center mb-5">
-            <img width="400px" src="http://alex.www.femlight.com/customize/storage/femlight-logo-02_grayPink.png" />
-          </div>
-        </v-flex> -->
-        <v-card flat class="elevation-12">
-          <v-toolbar flat color="white">
-            <v-toolbar-title>Create Your Account</v-toolbar-title>
-          </v-toolbar>
-          <v-card-text>
-            <v-form>
-              <v-text-field prepend-icon="person" name="fullname" label="Full Name" type="text" />
-              <v-text-field prepend-icon="mail" name="email" label="Email" type="email" />
-              <v-text-field id="password" prepend-icon="lock" name="password" label="Password" type="password" />
-            </v-form>
-          </v-card-text>
-          <v-card-actions>
-            <v-btn
-              block
-              color="primary"
-              @click="$router.push('/registration')"
-            >
-              Login
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <div
+    class="text-xs-center pa-3">
+    <v-card
+      flat
+      color="transparent"
+      style="width:500px;margin:auto;"
+      class="mt-5">
+      <v-toolbar flat color="transparent">
+        <v-toolbar-title class="display-1 font-weight-thin text-xs-center">
+          Create Your Account
+        </v-toolbar-title>
+      </v-toolbar>
+      <v-card-text>
+        <v-form>
+          <v-text-field prepend-icon="person" name="fullname" label="Full Name" type="text" />
+          <v-text-field prepend-icon="mail" name="email" label="Email" type="email" />
+          <v-text-field id="password" prepend-icon="lock" name="password" label="Password" type="password" />
+        </v-form>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn
+          block
+          color="primary"
+          @click="$router.push('/registration')"
+        >
+          Login
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
 </template>
 
 <script>
