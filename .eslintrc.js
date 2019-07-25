e4f9af669767155e2c1ef50199ec4cbb853626fc
Michael Giannini <mgiannini@mgiannini-iMac-2.ilf.com>
2019-07-25 17:38:06 -0500

    removed eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,33 @@
-module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  extends: [
-    // 'prettier',
-    // 'prettier/vue',
-    // 'plugin:prettier/recommended',
-    'plugin:vue/recommended',
-    'standard',
-    // '@nuxtjs' //,
-    // 'plugin:nuxt/recommended'
-  ],
-  plugins: [
-    'vue'
-    // 'prettier'
-  ],
-  // add your custom rules here
-  rules: {
-    'nuxt/no-cjs-in-config': 'off',
-    'no-console': 'off',
-    'no-tabs': 'off',
-    'no-mixed-spaces-and-tabs': 'off',
-    'vue/no-use-v-if-with-v-for': 'off',
-    'vue/no-use-v-if-with-v-for': 'off',
-    'vue/require-default-prop': 'off'
-  }
-}
+// module.exports = {
+//   root: true,
+//   env: {
+//     browser: true,
+//     node: true
+//   },
+//   parserOptions: {
+//     parser: 'babel-eslint'
+//   },
+//   extends: [
+//     // 'prettier',
+//     // 'prettier/vue',
+//     // 'plugin:prettier/recommended',
+//     'plugin:vue/recommended',
+//     'standard',
+//     // '@nuxtjs' //,
+//     // 'plugin:nuxt/recommended'
+//   ],
+//   plugins: [
+//     'vue'
+//     // 'prettier'
+//   ],
+//   // add your custom rules here
+//   rules: {
+//     'nuxt/no-cjs-in-config': 'off',
+//     'no-console': 'off',
+//     'no-tabs': 'off',
+//     'no-mixed-spaces-and-tabs': 'off',
+//     'vue/no-use-v-if-with-v-for': 'off',
+//     'vue/no-use-v-if-with-v-for': 'off',
+//     'vue/require-default-prop': 'off'
+//   }
+// }
